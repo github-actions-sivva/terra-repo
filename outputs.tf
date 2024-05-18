5bec0fb14eecfb49cfecb85dc6f005de044158cf
Siva <siva@gmail.com>
2024-05-18 17:46:45 +0530

    added outout

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "infra_svc_account_email" {
+  description = "Email of the Infrastructure Service Account"
+  value       = google_service_account.tf_infra_svc_account.email
+}
+
+output "deploy_svc_account_email" {
+  description = "Email of the Deployment Service Account"
+  value       = google_service_account.tf_deploy_svc_account.email
+}
