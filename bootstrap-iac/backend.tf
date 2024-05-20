c4b7ea657833b1095bbe71ce730901e8cd26d13d
Siva <siva@gmail.com>
2024-05-20 11:38:47 +0530

    added bucket

--- bootstrap-iac/backend.tf
+++ bootstrap-iac/backend.tf
@@ -4,11 +4,11 @@ terraform {
       source  = "hashicorp/google"
       version = "5.28.0"
     }
-  }
+  } /*
   backend "gcs" {
     bucket = "quantum-weft-420714-gmek"
     prefix = "terraform/state"
-  }
+  }*/
 }
 
 provider "google" {
